786f9106dbc3e8d796b8aec2f07715dc81b92dd6
Braxton Chatman <braxton.a.chatman@gmail.com>
2023-07-09 17:29:07 -0700

    format display and fix bugs

--- blackjack.py
+++ blackjack.py
@@ -3,33 +3,32 @@
 This script runs a simulation of a game of Blackjack
 """
 from random import randint
-shuffled_deck = []
-discard_deck = []
 card_remainder_set = 50
 deck_number_set = 8
 payout_rate_set = 1.5
-
+shuffled_deck = []
+discard_deck = []
 
 class Player:
     """A class representing a player in a blackjack game and their moves"""
-    card_values = dict([(i, i) for i in range(2,11)] + [(11, 10), (12, 10), (13, 10), (1, 11)])
+    card_values = dict([(i, i) for i in range(2,11)] + [(11, 10), (12, 10), (13, 10), (1, 11), (0, 0)])
 
     def __init__(self, name, input_cash = 100):
-        self.hand = []
+        self.hand = [0]
         self.play_cash = input_cash
         self.initial_cash = input_cash
         self.wager_amount = 0
-        self.hand_value = []
+        self.hand_value = [0]
         self.bust = False
         self.stayed = False
         self.name = name
 
     def make_wager(self):
         """prompt user for wager_amount, validate, and set"""
-        print(self.name, "please input the amount you would like to wager for the round: ")
-        wager_input = input()
+        self.print_player()
+        wager_input = input("Input wager: ")
         while not is_int(wager_input) or float(wager_input) < 1 or float(wager_input) > self.play_cash:
-            print("\Current cash: ", self.play_cash)
+            print("\nCurrent cash: ", self.play_cash)
             if not is_float(wager_input):
                 print("Please input a valid wager amount: ", end = "")
             elif float(wager_input) < 1:
@@ -40,11 +39,44 @@ class Player:
                 print("Your wager has been rounded down to the nearest dollar.\n")
                 break
             wager_input = input()
+        self.play_cash -= int(float(wager_input))
         self.wager_amount = int(float(wager_input))
-        input("Press Enter to continue...")
+
+    def prompt_move(self):
+        """prompt user for the move they would like to make on their turn"""
+        while not self.stayed and not self.bust:
+            self.print_player()
+            print("\nPlease select the option you would like to do: ")
+            print("1. Hit")
+            print("2. Stay")
+            player_move_input = input()
+
+            # input validation loop
+            while player_move_input.strip().lower() not in ["1", "2", "h", "hit", "s", "stay"]:
+                print("\nPlease input valid selection: ")
+                print("1. Hit")
+                print("2. Stay")
+                player_move_input = input()
+            player_move_input = player_move_input.strip().lower()
+
+            # hit
+            if player_move_input in ["1", "h", "hit"]:
+                if self.hand_value[0] < 21:
+                    self.hit()
+                else:
+                    print("You may not hit. You must stay.")
+                    self.stay()
+
+            # stay
+            elif player_move_input in ["2", "s", "stay"]:
+                print("\nYou stayed.\n")
+                self.stay()
+        self.stayed = False 
 
     def hit(self):
         """deal card to player hand"""
+        global shuffled_deck
+        global discard_deck
         if len(shuffled_deck) < card_remainder_set:
             shuffled_deck += discard_deck
             discard_deck = []
@@ -54,23 +86,29 @@ class Player:
         new_card = shuffled_deck[deck_index]
         shuffled_deck.pop(deck_index)
 
-        # add new card to hand value list. append if new card is ace
-        for i in range(0, len(self.hand_value) - 1):
+        # add new card to hand value. append if new card is ace
+        for i in range(0, len(self.hand_value)):
             self.hand_value[i] += self.card_values[new_card]
         if new_card == 1:
-            if i == 0:
+            if len(self.hand_value) == 1:
                 self.hand_value.append(0)
             self.hand_value[1] = self.hand_value[0] - 10
         if len(self.hand_value) == 2 and self.hand_value[0] > 21:
             self.hand_value.pop(0)
 
         # add new card to hand
+        if self.hand[0] == 0:
+            self.hand.pop(0)
         self.hand.append(new_card)
 
         # lose hand if bust
         if self.hand_value[0] > 21:
             self.bust = True
-            self.lose_hand()
+            if self.play_cash != -1:
+                print("\nYou busted.\n")
+                self.lose_hand()
+            else:
+                print("\nDealer Busted\n")
 
     def stay(self):
         """pass turn to next player"""
@@ -90,46 +128,62 @@ class Player:
         self.play_cash += self.wager_amount * payout_rate_set
         self.wager_amount = 0
         self.discard()
+        print("{}, you won!\n".format(self.name))
 
     def lose_hand(self):
         """lose wager to dealer and discard hand"""
         self.wager_amount = 0
         self.discard()
+        print("{}, you lost.\n".format(self.name))
 
     def push_hand(self):
         """tie with dealer and discard hand"""
         self.play_cash += self.wager_amount
         self.wager_amount = 0
         self.discard()
+        print("{}, you tied.\n".format(self.name))
 
     def discard(self):
         """empty hand to discard pile"""
+        global discard_deck
         discard_deck += self.hand
-        self.hand = []
-        self.hand_value = []
+        self.hand = [0]
+        self.hand_value = [0]
 
     def print_player(self):
         """print player cash, wager, and current hand to screen"""
+        print("\n")
         print(self.name)
+        print("~" * 12)
         print("Cash:  ", self.play_cash)
         print("Wager: ", self.wager_amount)
-        print("Hand: ")
-        for i in self.hand:
-            print(self.card_values[i], end = "  ")
+        print("Hand: ", end = " ")
+        if self.hand[0] != 0:
+            print(self.card_values[self.hand[0]], end = "")
+            for i in self.hand[1:]:
+                print(", ", self.card_values[i], end = "")
+        print("\nValue: ", self.hand_value[0])
 
 
 class Dealer(Player):
     """class representing dealer in blackjack"""
     def print_dealer(self):
         """print dealer top card while leaving bottom card hidden"""
-        print("Dealer Card: \n", self.hand[0])
-        
+        print("\n\nDealer")
+        print("~" * 12)
+        print("Top Card: {}\n".format(self.card_values[self.hand[0]]))
+        input("Press Enter to continue...")
 
+        
     def show_hand(self):
         """print dealer hand"""
-        print("Dealer Hand: ")
-        for i in self.hand:
-            print(self.card_values[i], end="  ")
+        print("\n\nDealer")
+        print("~" * 12)
+        print("Hand: ", end = " ")
+        print(self.hand[0], end = "")
+        for i in self.hand[1:]:
+            print(", ", self.card_values[i], end="")
+        print("\nValue: ", self.hand_value[0])
       
 
 def display_rules():
@@ -169,27 +223,26 @@ def is_int(string_value):
 
 def play_game():
     """play blackjack simulation"""
-    dealer = Dealer(0)
+    dealer = Dealer("Dealer", -1)
     num_players = ""
     player_list = []
     player_quit_list = []
     cash_input = ""
-    player_move_input = ""
     player_name_input = ""
     player_quit_input = ""
-    wager_input = ""
-
-
-    # allow selection for number of players from 1 to 7
-    print("Please select the number of players that would like to play (1-7): ")
-    num_players = input()
-    while num_players.strip() not in ['1', '2', '3', '4', '5', '6', '7']:
-        print("Please input a valid number of players: ")
-        num_players = input()
+    global shuffled_deck
+    global discard_deck
 
     # insert cards into shuffled_deck based on number of decks setting
     for i in range(1, 14):
-        shuffled_deck += [i] * deck_number_set
+        shuffled_deck += [i] * 4 * deck_number_set
+
+    # allow selection for number of players from 1 to 7
+    num_players = input("\nPlease select the number of players that would like to play (1-7): ")
+    while num_players.strip().lower() not in ['1', '2', '3', '4', '5', '6', '7',
+                                              'one', 'two', 'three', 'four', 'five',
+                                              'six', 'seven']:
+        num_players = input("Please input a valid number of players: ")
 
     # initialize each Player instance with prompted initial cash value and name into player list
     for player_number in range(1, int(num_players)+1):
@@ -197,11 +250,11 @@ def play_game():
         cash_input = input("Player {}, please input the amount of cash you would like to play with: ".format(player_number))
         while not is_int(cash_input) or float(cash_input) < 1:
             if not is_float(cash_input):
-                print("\nPlease input a valid amount:")
+                print("Please input a valid amount:", end = " ")
             elif float(cash_input) < 1:
-                print("\nSorry, we do not provide credit, please input a positive amount:")
+                print("Sorry, we do not provide credit, please input a positive amount:", end = " ")
             else:
-                print("\nWe only play with whole dollars, please keep your change.")
+                print("We only play with whole dollars, please keep your change.")
                 break
             cash_input = input()
         player_list.append(Player(player_name_input, int(float(cash_input))))
@@ -209,46 +262,30 @@ def play_game():
     # show players and cash
     for player in player_list:
         player.print_player()
-    print("Press Enter to continue...")
-    input()
+    input("\nPress Enter to continue...")
+    print("-" * 70)
 
     # game loops while there are any players
     while player_list != []:
-       
-        # wager prompt and validation loop
         for player in player_list:
             player.make_wager()
-
-            print("{}, please input the amount you would like to wager for the round: ".format(player.name))
-            wager_input = input()
-            while not is_int(wager_input) or float(wager_input) < 1 or float(wager_input) > player.play_cash:
-                print("\Current cash: ", player.play_cash)
-                if not is_float(wager_input):
-                    print("Please input a valid wager amount: ", end = "")
-                elif float(wager_input) < 1:
-                    print("That's not how we do wagers here, you must wager at least a dollar: ", end = "")
-                elif float(wager_input) > player.play_cash:
-                    print("You may not wager more than you have, please make a new wager: ", end = "")
-                else:
-                    print("Your wager has been rounded down to the nearest dollar.\n")
-                    break
-                wager_input = input()
-            player.set_wager(int(float(wager_input)))
-            input("Press Enter to continue...")
 
         # deal cards to players and dealer, shuffling if too few cards remain
         for i in range(2):
             for player in player_list:
                 player.hit()
             dealer.hit()
+        print("-" * 70)
 
         # show player hands and dealer's face up card
         for player in player_list:
             player.print_player()
         dealer.print_dealer()
+        print("-" * 70)
 
         # if dealer has natural, show hand, and all players without blackjack lose
         if dealer.hand_value == 21:
+            print("Dealer has 21\n")
             dealer.show_hand()
             for player in player_list:
                 if player.hand_value == 21:
@@ -259,52 +296,21 @@ def play_game():
         # dealer does not have natural, players take turns
         else:
             for player in player_list:
-                while not player.stayed and not player.bust:
-                    print("{}: ".format(player.name))
-                    player.print_player()
-                    print("Please select the option you would like to do: ")
-                    print("1. Hit")
-                    print("2. Stay")
-                    player_move_input = input()
-
-                    # input validation loop
-                    while player_move_input.strip().lower() not in ["1", "2", "h", "hit", "s", "stay"]:
-                        print("Please input valid selection: ")
-                        print("1. Hit")
-                        print("2. Stay")
-                        player_move_input = input()
-                    player_move_input = player_move_input.strip().lower()
-
-                    # hit
-                    if player_move_input in ["1", "h", "hit"]:
-                        if player.hand_value[0] < 21:
-                            player.print_player()
-                            player.hit()
-                        else:
-                            print("You may not hit. You must stay.")
-                            player.stay()
-
-                    # stay
-                    elif player_move_input in ["2", "s", "stay"]:
-                        print("You stayed.")
-                        player.stay()     
-                for player in player_list:
-                    player.stayed = False
-
+                player.prompt_move() 
+                print("-" * 70)   
+             
             # dealer turn
             dealer.show_hand()
-            while dealer.hand_value < 17:
+            while dealer.hand_value[0] < 17 and not dealer.bust:
                 dealer.hit()
                 dealer.show_hand()
-            input("Press Enter to continue...")
+            input("\nPress Enter to continue...")
+            print()
 
             # resolve player wagers after dealer turn
             for player in player_list:
-                # dealer busts
                 if dealer.bust and not player.bust:
                     player.win_hand()
-                
-                # dealer doesn't bust
                 elif not player.bust:
                     if player.hand_value[0] > dealer.hand_value[0]:
                         player.win_hand()
@@ -322,18 +328,19 @@ def play_game():
             player.print_player()
 
         # exit player from game if cash is 0, prompt player if they would like to exit
-        for i in range(0, len(player_list)-1):
+        for i in range(0, len(player_list)):
             player_list[i].print_player()
             if player_list[i].play_cash == 0:
                 input("{}, you have run out of money, you must leave the table.".format(player_list[i].name))
                 player_quit_list.append(i)
             else:
-                player_quit_input = input("{}, Press Enter to continue, or input '1' if you would like to quit...".format(player_list[i].name))
+                player_quit_input = input("{}, Press Enter to continue,\nor input '1' if you would like to quit... ".format(player_list[i].name))
                 if player_quit_input.strip().lower() in ["1", "q", "quit", "exit", "leave", "done"]:
                     player_quit_list.append(i)
-                    input("Thank you for playing {}.\nPress Enter to continue...".format(player_list[i].name))
-        for i in player_quit_list.reverse():
-            player_list.pop(i)   
+                    input("\nThank you for playing {}, goodbye.\n".format(player_list[i].name))
+        if player_quit_list != []:
+            for i in player_quit_list.reverse():
+                player_list.pop(i)   
         player_quit_list = []
 
 
@@ -345,14 +352,19 @@ def main():
         print("1. Play")
         print("2. Rules")
         print("3. Settings")
-        print("4. Exit\n")
+        print("4. Exit")
         
         selection_choice = ""
         selection_choice = input()
         while selection_choice.lower() not in ['1', '2', '3', '4', 'p', 'play',
                                                'r', 'rule', 'rules', 's', 'set',
                                                'setting', 'settings', 'e', 'exit']:
+            print()
             print("Please make a valid selection...")
+            print("1. Play")
+            print("2. Rules")
+            print("3. Settings")
+            print("4. Exit")
             selection_choice = input()
         
         if selection_choice.lower() in ['1', 'p', 'play']:
@@ -368,14 +380,14 @@ def main():
             selection_choice = ""
 
         if selection_choice.lower() in ['4', 'e', 'exit']:
-            print("Thank you for playing\n")
+            print("\nThank you for playing! Come back again!\n")
             exit()
 
    
 if __name__ == '__main__':
     main()
 
-
+# TODO: add messages to win, lose, push
 # TODO: write display_rules function
 # TODO: update printing and hitting and card dict to accomodate suit
 
@@ -384,6 +396,5 @@ if __name__ == '__main__':
 # TODO: payout rate changes in win for blackjack vs not
 
 
-# UPDATE: write win, lose, push, and fix discard methods
-# UPDATE: write print player method
-# UPDATE: write dealer print methods
+#UP: menu layout in main
+#UP: player print function
